1ec8cbb081adf5bf7c73abb6ada942dfa9e1ca80
Rob Attfield <emailme@robertattfield.com>
2019-08-17 20:48:19 +1200

    Testing Shodan service provider.

--- src/ShodanServiceProvider.php
+++ src/ShodanServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace RattfieldNz\Shodan;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
+use RattfieldNz\SafeUrls\SafeUrls;
 
 class ShodanServiceProvider extends ServiceProvider
 {
@@ -22,8 +24,77 @@ class ShodanServiceProvider extends ServiceProvider
             'shodan'
         );
 
+        $this->app->alias(Shodan::class, 'shodan');
+
         $this->app->bind('shodan', function () {
             return new Shodan();
         });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['shodan'];
+    }
+
+    /**
+     * Console-specific booting.
+     *
+     * @return void
+     */
+    public function bootForConsole()
+    {
+        // Publishing the configuration file.
+        $this->publishes([
+            __DIR__.'/../config/shodan.php' => config_path('shodan.php'),
+        ], 'shodan');
+
+        // Registering package commands.
+        $this->commands(['shodan']);
+    }
+
+    /**
+     * Merge the given configuration with the existing configuration.
+     *
+     * @param string $path
+     * @param string $key
+     *
+     * @return void
+     */
+    protected function mergeConfigFrom($path, $key)
+    {
+        $config = $this->app['config']->get($key, []);
+        $this->app['config']->set($key, $this->mergeConfig($config, require $path));
+    }
+
+    /**
+     * Merges the configs together and takes multi-dimensional arrays into account.
+     *
+     * @param array $original
+     * @param array $merging
+     *
+     * @return array
+     */
+    protected function mergeConfig(array $original, array $merging)
+    {
+        $array = array_merge($original, $merging);
+        foreach ($original as $key => $value) {
+            if (!is_array($value)) {
+                continue;
+            }
+            if (!Arr::exists($merging, $key)) {
+                continue;
+            }
+            if (is_numeric($key)) {
+                continue;
+            }
+            $array[$key] = $this->mergeConfig($value, $merging[$key]);
+        }
+
+        return $array;
     }
 }
---
+++ tests/ShodanServiceProviderTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace RattfieldNz\Shodan\Tests;
+
+use Illuminate\Foundation\Application;
+use Mockery;
+use Mockery\MockInterface;
+use RattfieldNz\Shodan\Shodan;
+use RattfieldNz\Shodan\ShodanServiceProvider;
+
+/**
+ * Class ShodanServiceProviderTest.
+ */
+class ShodanServiceProviderTest extends TestCase
+{
+    /**
+     * @var MockInterface
+     */
+    protected $appMock;
+
+    /**
+     * @var MockInterface
+     */
+    protected $config;
+
+    /**
+     * @var ShodanServiceProvider
+     */
+    protected $provider;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp():void
+    {
+        parent::setUp();
+
+        $this->config = Mockery::mock();
+
+        $this->appMock = Mockery::mock(Application::class);
+
+        $this->provider = new ShodanServiceProvider($this->app);
+    }
+
+    public function testServiceProviderIsWorking()
+    {
+        $this->appMock->shouldReceive('singleton')->with(
+            'safe-urls',
+            Mockery::type('Closure')
+        )->andReturnNull();
+
+        $this->appMock->shouldReceive('alias')->with(
+            Shodan::class, 'shodan'
+        )->andReturnNull();
+
+        $this->assertNull($this->provider->register());
+    }
+
+    public function testProvides()
+    {
+        $this->appMock->shouldReceive('provides')->andReturn('shodan');
+        $this->assertEquals(['shodan'], $this->provider->provides());
+    }
+
+    /**
+     * Test boot provider.
+     */
+    public function testBoot()
+    {
+        $this->assertNull($this->provider->boot());
+    }
+
+    public function testBootForConsole()
+    {
+        $this->appMock->shouldReceive('publishes')
+            ->zeroOrMoreTimes()
+            ->with('path.config')
+            ->andReturn('/some/config/path');
+
+        $this->appMock->shouldReceive('commands')
+            ->zeroOrMoreTimes()
+            ->with(['shodan'])
+            ->andReturnNull();
+
+        $this->assertNull($this->provider->bootForConsole());
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+}
