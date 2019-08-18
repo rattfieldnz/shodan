f8ee755412c6a4db3aa8ce2130686f097a8434ec
Rob Attfield <emailme@robertattfield.com>
2019-08-18 00:56:21 +0000

    Apply fixes from StyleCI

--- src/Facades/ShodanFacade.php
+++ src/Facades/ShodanFacade.php
@@ -5,14 +5,12 @@ namespace RattfieldNz\Shodan\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * Class ShodanFacade
+ * Class ShodanFacade.
  *
  * @category PHP
  *
  * @author  Rob Attfield <emailme@robertattfield.com>
  * @license https://github.com/rattfieldnz/shodan/blob/master/LICENSE MIT
- *
- * @package RattfieldNz\Shodan\Facades
  */
 class ShodanFacade extends Facade
 {
