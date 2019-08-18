6b5f6b81c7f180f4690c7f7fb67c830ac28b69f5
Rob Attfield <emailme@robertattfield.com>
2019-08-18 14:30:25 +1200

    Initial creation and testing of Data class.

---
+++ src/Libraries/Data/Data.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace RattfieldNz\Shodan\Libraries\Data;
+
+use RattfieldNz\Shodan\Libraries\Config\Config;
+
+/**
+ * Class Data.
+ *
+ * @category PHP
+ *
+ * @author  Rob Attfield <emailme@robertattfield.com>
+ * @license https://github.com/rattfieldnz/shodan/blob/master/LICENSE MIT
+ */
+class Data
+{
+    private $_ip;
+    private $_url;
+
+    public function __construct(string $url)
+    {
+        $this->_url = $url;
+        $this->_ip = $this->getIp($this->_url);
+    }
+
+    /**
+     * Get Shodan API URL with key.
+     *
+     * @return string
+     */
+    public function shodanApiUrl()
+    {
+        return 'https://api.shodan.io/shodan/host/' . $this->_ip . '?key='.
+            Config::shodanApiKey();
+    }
+
+    /**
+     * Get IPv4 address from a URL.
+     *
+     * @param  string $url
+     * @return string|null
+     */
+    public function getIp(string $url): string
+    {
+        $domain = parse_url($url)['host'];
+        $ip = gethostbyname($domain);
+
+        return filter_var($ip, FILTER_VALIDATE_IP) ? $ip : null;
+    }
+
+    /**
+     * Returns URL set when initialising Data object.
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->_url;
+    }
+}
---
+++ tests/Libraries/Data/DataTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RattfieldNz\Shodan\Tests\Libraries\Data;
+
+use RattfieldNz\Shodan\Libraries\Config\Config;
+use RattfieldNz\Shodan\Libraries\Data\Data;
+use RattfieldNz\Shodan\Tests\TestCase;
+
+class DataTest extends TestCase
+{
+    private $_url;
+    private $_data;
+    private $_mock;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->_url = 'https://www.robertattfield.com';
+
+        $this->_data = new Data($this->_url);
+
+        $this->assertEquals($this->_url, $this->_data->getUrl());
+
+    }
+
+    public function testShodanApiUrl()
+    {
+        $expected = 'https://api.shodan.io/shodan/host/104.28.26.171?key='.
+            Config::shodanApiKey();
+        $actual = $this->_data->shodanApiUrl();
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGetIp()
+    {
+        $expected = '104.28.26.171';
+        $actual = $this->_data->getIp($this->_url);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGetUrl()
+    {
+        $expected = $this->_url;
+        $actual = $this->_data->getUrl();
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+}
