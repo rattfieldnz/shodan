e3a9dbe55835fc62e0b525451e32dbadae7cbf5e
Rob Attfield <emailme@robertattfield.com>
2019-08-19 20:16:19 +1200

    Adding tests for ShodanFacade.

---
+++ tests/Facades/ShodanFacadeTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace RattfieldNz\Shodan\Tests\Facades;
+
+use Illuminate\Foundation\Application;
+use Prophecy\Prophet;
+use RattfieldNz\Shodan\Facades\ShodanFacade;
+use RattfieldNz\Shodan\Shodan;
+use RattfieldNz\Shodan\Tests\TestCase;
+
+class ShodanFacadeTest extends TestCase
+{
+    protected $app;
+
+    protected $prophet;
+
+    protected function setUp():void
+    {
+        parent::setUp();
+
+        $this->prophet = new Prophet();
+
+        $this->app = $this->prophet->prophesize(Application::class);
+
+        $this->app->offsetGet('shodan')
+            ->willReturn((new Shodan()));
+
+        ShodanFacade::setFacadeApplication($this->app->reveal());
+    }
+
+    public function testFacadesWorksAsExpected()
+    {
+        $this->assertInstanceOf(
+            Shodan::class,
+            ShodanFacade::getFacadeRoot()
+        );
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+
+}
